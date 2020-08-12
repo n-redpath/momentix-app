f832c18acb302a2d3f2e6de89dbc9aaec132edf9
Nina Redpath <ninaredpath@gmail.com>
2020-08-12 16:38:54 -0700

    assets package
    the assets folder didnt upload the first time

---
+++ Wireframes part 1/Assets/Scripts/lastSlide.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+
+public class lastSlide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    public void previous () 
+    {
+        Debug.Log("Button Pressed");   
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
